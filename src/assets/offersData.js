f8d5eaf9f6a87019a52ee120e086a067c8207338
joan <joanmutti1090@gmail.com>
2022-03-10 22:03:54 -0300

    favicon

--- src/assets/offersData.js
+++ src/assets/offersData.js
@@ -6,13 +6,6 @@ import tapa from "../tapa.png";
 
 let desc =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias vitae, alias asperiores nam a totam animi aperiam exercitationem dicta perferendis eius sint, ullam saepe sunt quos! Esse, aperiam dolor? ";
-const offersData = [
-    {img: pechugas, name: 'Pechuga de Pollo', price: 600, offer: '1 Kilo', id: 'iddeprueba', description: desc, inOffer: true, type: ["polleria", "productos"] , soldFor: 'Kg'},
-    {img: soja, name: 'Milanesas de soja', price: 299, offer: '1 Kilo', id: 'iddeprueba2', description: desc, inOffer: true, type: ["vegetariano", "congelados", "productos"], soldFor: 'Kg'},
-    {img: tapa, name: 'Tapa de asado', price: 790, offer: '1 Kilo', id: 'iddeprueba3', description: desc, inOffer: true, type:[ "carniceria", "productos"], soldFor: 'Kg'},
-    {img: alitas, name: 'Alitas de Pollo', price: 200, offer: '2 Kilos', id: 'iddeprueba4',description: desc, inOffer: false, type: ["polleria", "congelados", "productos"], soldFor: 'Kg'},
-    {img: bastones, name: 'Bastones de queso', price: 830, offer: '1 Kilo', id: 'iddeprueba5', description: desc, inOffer: false, type: ["vegetariano", "congelados", "productos"], soldFor: 'Kg'},
-]
 
 const offersData = [
   {
@@ -24,6 +17,7 @@ const offersData = [
     description: desc,
     inOffer: true,
     type: ["polleria", "productos"],
+    soldFor: "Kg",
   },
   {
     img: soja,
@@ -34,6 +28,7 @@ const offersData = [
     description: desc,
     inOffer: true,
     type: ["vegetariano", "congelados", "productos"],
+    soldFor: "Kg",
   },
   {
     img: tapa,
@@ -44,6 +39,7 @@ const offersData = [
     description: desc,
     inOffer: true,
     type: ["carniceria", "productos"],
+    soldFor: "Kg",
   },
   {
     img: alitas,
@@ -54,6 +50,7 @@ const offersData = [
     description: desc,
     inOffer: false,
     type: ["polleria", "congelados", "productos"],
+    soldFor: "Kg",
   },
   {
     img: bastones,
@@ -64,6 +61,7 @@ const offersData = [
     description: desc,
     inOffer: false,
     type: ["vegetariano", "congelados", "productos"],
+    soldFor: "Kg",
   },
 ];
 
--- src/components/productsComponents/ProductCount.jsx
+++ src/components/productsComponents/ProductCount.jsx
@@ -1,38 +1,40 @@
-import React ,{useState, useEffect} from 'react'
-import { CgMathPlus, CgMathMinus } from 'react-icons/cg'
-import AddProductCart from '../AddProduct/AddProductCart'
+import React, { useState, useEffect } from "react";
+import { CgMathPlus, CgMathMinus } from "react-icons/cg";
+import AddProductCart from "../AddProduct/AddProductCart";
 
-const ProductsCount = ({ initial = 0, product}) => {
-  
-    const [count, setCount] = useState(initial)
-  
-    useEffect(()=> {
-      setCount(initial)
-    },[])
+const ProductsCount = ({ initial = 0, product }) => {
+  const [count, setCount] = useState(initial);
 
-    const plus = () =>{
-        setCount(count + 0.25)
-       
+  useEffect(() => {
+    setCount(initial);
+  }, []);
+
+  const plus = () => {
+    setCount(count + 0.25);
+  };
+  const minus = () => {
+    if (count > initial) {
+      setCount(count - 0.25);
     }
-    const minus = () =>{
-      if(count > initial) {
-        setCount( count - 0.25)
-        
-      }
-    }
+  };
 
-
-    return ( 
-      <div className='d-flex justify-content-center align-items-center row'>
-      <div className=' container container-fluid d-flex justify-content-between align-items-center' id="btnCount">
-        <button className='btn ' id='btn-minus' onClick={minus}>-</button>
-        <p className=' p-count text-center' >{count}</p>
-        <button className='btn 'id='btn-plus' onClick={plus}>+</button>
+  return (
+    <div className="d-flex justify-content-center align-items-center row">
+      <div
+        className=" container container-fluid d-flex justify-content-between align-items-center"
+        id="btnCount"
+      >
+        <button className="btn " id="btn-minus" onClick={minus}>
+          -
+        </button>
+        <p className=" p-count text-center">{count}</p>
+        <button className="btn " id="btn-plus" onClick={plus}>
+          +
+        </button>
       </div>
-      <AddProductCart product={product} cant={count}/>
-      </div>
-  )
- 
-}
+      <AddProductCart product={product} cant={count} />
+    </div>
+  );
+};
 
-export default ProductsCount
+export default ProductsCount;

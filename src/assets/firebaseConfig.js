9bb64b0c56d1f554856595bfc5850d5afbb24505
joan <joanmutti1090@gmail.com>
2022-03-14 20:52:34 -0300

    env variables

---
+++ src/assets/firebaseConfig.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+console.log(import.meta.env);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+let envProcess;
+
+import.meta?.env ? (envProcess = import.meta.env) : (envProcess = process.env);
+
+const firebaseConfig = {
+  apiKey: envProcess.VITE_API_KEY,
+  authDomain: envProcess.VITE_AUTH_DOMAIN,
+  projectId: envProcess.VITE_PROJECT_ID,
+  storageBucket: envProcess.VITE_STORAGE_BUCKET,
+  messagingSenderId: envProcess.VITE_MESSAGING_SENDER_ID,
+  appId: envProcess.VITE_ID,
+  measurementId: envProcess.VITE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+// Inicializado el Google Analytics por si se quiere usar
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+export default db;

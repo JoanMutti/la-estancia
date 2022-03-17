53620950c6f58a41bdd17624227e150dab7f3026
artebleker <artebleker@gmail.com>
2022-03-17 10:02:20 -0300

    textos

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -4,10 +4,7 @@ import './AboutUs.css'
 import imgFondo from '../../Fondo_about.png'
 
 const AboutUs = () => {
-  let title = "Nuestra Historia";
-  let text =
-    " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut modi cum iste, impedit quas placeat nihil quam sequi reiciendis molestiae assumenda provident voluptatem recusandae temporibus, fugit fugiat quibusdam praesentium veritatis!";
-
+ 
   return (
     <>
       <div
@@ -17,8 +14,13 @@ const AboutUs = () => {
       <Layout>
         <div className="aboutUs">
           <div className="aboutUs-card container container-fluid text-center">
-            <h1 className="fs-1 aboutUs-title">{title}</h1>
-            <p className="aboutUs-text">{text}</p>
+            <h1 className="fs-1 aboutUs-title">Sobre Nosotros</h1>
+            <p className="aboutUs-text fs-4">MISIÓN</p>
+            <p className="aboutUs-text">Ofrecer servicios y productos con certificación de estándares de calidad, revalorizando y priorizando la empatía, el buen trato y el respeto para con los clientes.</p>
+            <p className="aboutUs-text fs-4">VISIÓN</p>
+            <p className="aboutUs-text">Reconstruir, desde nuestra actividad y a través del contacto con el cliente, aquellos valores de antaño centrados en el respeto al prójimo, el don de la gente y una cultura del trabajo que dignifica a la persona.</p>
+            <p className="aboutUs-text fs-4">VALORES</p>
+            <p className="aboutUs-text">La vocación de servicio, la empatía, el respeto, la consideración de la importancia de los detalles, la atención, la simpatía son algunos de los valores que la empresa quiere rescatar y acentuar.</p>
           </div>
         </div>
       </Layout>
